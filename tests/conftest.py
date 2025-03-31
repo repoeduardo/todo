4db495573613766b73c0d7a9cedf4d1e1b9d8776
Eduardo Araujo Da Silva <eduardoaraujodasilva@protonmail.com>
2025-03-31 15:02:21 -0300

    implanting session and a new fixture for database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 from todo.main import app
+from todo.models import table_registry
 from fastapi.testclient import TestClient
 import pytest
 
 @pytest.fixture
 def client():
     return TestClient(app) # arrange
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+
+    # context manager
+    with Session(engine) as session:
+        yield session # yield return session before destroy the registry
+
+    table_registry.metadata.drop_all(engine)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,14 @@
 from todo.models import User
+from sqlalchemy import  select
 
-def test_create_user():
+def test_create_user(session):
+   
     user = User(username='test', password='secret', email='test@mail.com')
-    assert user.password == 'secret'
+    session.add(user)
+    session.commit()
+    #session.refresh(user)
+    user_obj = session.scalar(
+        select(User).where(User.email == 'test@mail.com')
+    )
+
+    assert user_obj.username == 'test'
